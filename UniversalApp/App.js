c1ed57f4b953dccb01cce2f8020eb88a72730a86
Mikaela <imchristsgir1@gmail.com>
2024-05-24 20:17:49 +0000

    Edit text component to "My name is YourName!"

--- UniversalApp/App.js
+++ UniversalApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is YourName</Text>
+      <Text>My name is YourName!</Text>
       <StatusBar style="auto" />
     </View>
   );
